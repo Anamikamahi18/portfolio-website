b8cf52127fa586a56f679888a4661e7cfaad106f
Anamika M <anamikamahi18@gmail.com>
2025-10-25 01:37:35 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import About from './components/About';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Experience from './components/Experience';
-import Contact from './components/Contact';
 import Footer from './components/Footer';
 
 function App() {
@@ -43,7 +42,6 @@ function App() {
           <Skills />
           <Projects />
           <Experience />
-          <Contact />
         </main>
         <Footer />
       </div>
